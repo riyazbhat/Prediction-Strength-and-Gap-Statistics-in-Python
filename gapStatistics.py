616670579d266ff4f19cc85f0c5393220e99994a
Riyaz <riyaz@ltrc.iiit.ac.in>
2014-07-03 21:54:34 +0530

    added new files to this repository

---
+++ gapStatistics.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python -*- coding: utf-8 -*-
+
+__Author__ = "Riyaz Ahmad Bhat"
+__Version__ = "1.0"
+
+def dispersion (data, k):
+    if k == 1:
+        cluster_mean = np.mean(data, axis=0)
+        distances_from_mean = np.sum((data - cluster_mean)**2,axis=1)
+        dispersion_val = np.log(sum(distances_from_mean))
+    else:
+        k_means_model_ = KMeans(n_clusters=k, max_iter=50, n_init=5).fit(data)
+        distances_from_mean = range(k)
+        for i in range(k):
+            distances_from_mean[i] = int()
+            for idx, label in enumerate(k_means_model_.labels_):
+                if i == label:
+                    distances_from_mean[i] += sum((data[idx] - k_means_model_.cluster_centers_[i])**2)
+        dispersion_val = np.log(sum(distances_from_mean))
+
+    return dispersion_val
+
+def reference_dispersion(data, num_clusters, num_reference_bootstraps):
+    dispersions = [dispersion(generate_uniform_points(data), num_clusters) for i in range(num_reference_bootstraps)]
+    mean_dispersion = np.mean(dispersions)
+    stddev_dispersion = float(np.std(dispersions)) / np.sqrt(1. + 1. / num_reference_bootstraps) 
+    return mean_dispersion
+
+def generate_uniform_points(data):
+    mins = np.argmin(data, axis=0)
+    maxs = np.argmax(data, axis=0)
+
+    num_dimensions = data.shape[1]
+    num_datapoints = data.shape[0]
+
+    reference_data_set = np.zeros((num_datapoints,num_dimensions))
+    for i in range(num_datapoints):
+        for j in range(num_dimensions):
+            reference_data_set[i][j] = random.uniform(data[mins[j]][j],data[maxs[j]][j])
+
+    return reference_data_set   
+
+def gap_statistic (data, nthCluster, referenceDatasets):
+    actual_dispersion = dispersion(data, nthCluster)
+    ref_dispersion = reference_dispersion(data, nthCluster, num_reference_bootstraps)
+    return actual_dispersion, ref_dispersion
+
+if __name__ == "__main__":
+
+    import random
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from sklearn.cluster import KMeans
+
+    data=np.loadtxt('iris.mat', delimiter=',', dtype=float)
+
+    maxClusters = 10
+    num_reference_bootstraps = 10
+    dispersion_values = np.zeros((maxClusters,2))
+
+    for cluster in range(1, maxClusters+1):
+        dispersion_values_actual,dispersion_values_reference = gap_statistic(data, cluster, num_reference_bootstraps)
+        dispersion_values[cluster-1][0] = dispersion_values_actual
+        dispersion_values[cluster-1][1] = dispersion_values_reference
+
+    gaps = dispersion_values[:,1] - dispersion_values[:,0]
+
+    print gaps
+    print "The estimated number of clusters is ", range(maxClusters)[np.argmax(gaps)]+1
+
+    plt.plot(range(len(gaps)), gaps)
+    plt.show()
---
+++ predictionStrength.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+
+def k_fold (data,shuffled):
+	testSample = list()        
+	trainingSample = list()
+	for i in range(data.shape[0]):
+		if i in shuffled:
+			testSample.append(data[i])
+		else:
+			trainingSample.append(data[i])
+	return np.array(trainingSample), np.array(testSample)
+
+def closest_center (point, centroids):
+	min_index = -1
+	min_distance = float('inf')
+	for i in range(len(centroids)):
+		center = centroids[i]
+		#print center,
+		#print point
+		d = sum((point - center)**2)
+		#print d
+		if d < min_distance:
+			min_index = i
+			min_distance = d
+
+	return min_index
+
+def calculate_prediction_strength (test_set, test_labels, training_centers, k):
+	clusterLength = test_labels.tolist().count(k)
+	if clusterLength <= 1:
+		return float('inf')
+	else:
+		count = 0.
+		for i in range(len(test_labels)-1): 
+			for j in range(i+1, len(test_labels)):
+				if test_labels[i] == test_labels[j] == k:
+					p1 = test_set[i]
+					p2 = test_set[j]                        
+					if (closest_center(p1, training_centers) == closest_center(p2, training_centers)):
+						count += 1
+		# Return the proportion of pairs that stayed in the same cluster.
+		#print count,(clusterLength * (clusterLength - 1) / 2.), clusterLength
+		count = count / (clusterLength * (clusterLength - 1) / 2.)
+	return count
+
+def prediction_strength_of_cluster (test_set, test_labels, training_centers, k):
+	prediction_strengths = [calculate_prediction_strength(test_set, test_labels, training_centers, i) for i in range(k)]
+	return min(prediction_strengths)	
+	
+if __name__ == "__main__":
+
+	import sys
+	import random
+	import numpy as np
+	import matplotlib.pyplot as plt
+	from sklearn.cluster import KMeans
+
+	#matrix=np.loadtxt('matrix1k.psp', delimiter=',', dtype=float)
+	try:
+		assert sys.argv[1]
+	except Exception,error:
+		print error,"Please specify the required argument(s)"
+		sys.exit(0)
+	else:
+		matrix=np.loadtxt(sys.argv[1], delimiter=',', dtype=float)
+
+	population = range(matrix.shape[0])
+	testSetLength = matrix.shape[0]/10
+	choice = random.sample(population, testSetLength)
+
+	maxK = 10
+	maxTrials = 3
+	prediction_strengths = np.zeros((maxTrials, maxK))
+	trainingSet, testingSet = k_fold (matrix,choice)
+	for trial in range(maxTrials):
+		#trainingSet, testingSet = k_fold (matrix,choice)
+		for k in range(1,maxK+1):
+			if k==1:
+				prediction_strengths[trial,k-1] = 1.
+			else:
+				testCluster = KMeans(n_clusters=k, max_iter=50, n_init=5).fit(testingSet)
+				trainingCluster = KMeans(n_clusters=k, max_iter=50, n_init=5).fit(trainingSet)
+
+				prediction_strengths[trial,k-1] = prediction_strength_of_cluster(testingSet,
+												 testCluster.labels_, trainingCluster.cluster_centers_, k)
+	
+	means = np.mean(prediction_strengths,0)
+	stddevs = np.std(prediction_strengths,0)
+
+	#We use 0.8 as our prediction strength threshold. 
+	#Find the largest number of clusters with a prediction strength greater than this threshold; 
+	#this forms our estimate of the number of clusters.
+	
+	print means
+	if max(means) > 0.8:
+		estimated = max([i for i,j in enumerate(means) if j > 0.8])+1                
+	else:
+		estimated = max(means)+1
+
+	print "The estimated number of clusters is ", estimated
+	
+	#print range(1,maxK+1), means
+	plt.plot(range(1,maxK+1),means)
+	plt.show()
